e7c5168b8e784a726834130aa4feadeb4792a554
eatsleepcoderepeat-gl <51761690+eatsleepcoderepeat-gl@users.noreply.github.com>
2021-05-17 20:21:27 -0400

    Pointed endpoints at production, updated attribution

--- README.md
+++ README.md
@@ -29,9 +29,9 @@ Cryptohopper may change their website at any moment, causing one or all of these
 
 ## all.user.js
 
-> By [@eatsleepcoderepeat-gl](https://github.com/eatsleepcoderepeat-gl) (Xerahn on Discord) and [@markrickert](https://github.com/markrickert)
+> By [@eatsleepcoderepeat-gl](https://github.com/eatsleepcoderepeat-gl) (Xerahn on Discord)
 
-* The one script to rule them all. This is a master script, provided for user convenience, which will install all other scripts available at the time of installation. If auto update is enabled, this will also keep all other scripts up to date as well and will add any new features (scripts) automatically. ⚠️ *If using this, there's no need to manually install individual scripts; any other installed scripts from this project should be uninstalled prior to installing this script.*
+* The one script to rule them all. This is a master script, provided for user convenience, which will load all other scripts available at the time of installation. If auto update is enabled, this will also keep all other scripts up to date as well and will add any new features (scripts) automatically. ⚠️ *If using this, there's no need to manually install individual scripts; any other installed scripts from this project should be uninstalled prior to installing this script.*
 
 ## coin-watchlist.user.js
 
--- all.user.js
+++ all.user.js
@@ -2,21 +2,21 @@
 // @name         Cryptohopper MASTER SCRIPT
 // @namespace    https://github.com/markrickert/cryptohopper-dashboard-watchlist
 // @version      0.1
-// @description  The one script to rule them all. This is a master script, provided for user convenience, which will install all other scripts available at the time of installation. If auto update is enabled, this will also keep all other scripts up to date as well and will add any new features (scripts) automatically. If using this, there's no need to manually install individual scripts; any other installed scripts from this project should be uninstalled prior to installing this script.
+// @description  The one script to rule them all. This is a master script, provided for user convenience, which will load all other scripts available at the time of installation. If auto update is enabled, this will also keep all other scripts up to date as well and will add any new features (scripts) automatically. If using this, there's no need to manually install individual scripts; any other installed scripts from this project should be uninstalled prior to installing this script.
 // @author       @markrickert, @eatsleepcoderepeat-gl, @henrygarle
 // @homepage     https://github.com/markrickert/cryptohopper-dashboard-watchlist
-// @updateURL    https://github.com/eatsleepcoderepeat-gl/cryptohopper-dashboard-watchlist/raw/dev/master-script/all.user.js
+// @updateURL    https://github.com/markrickert/cryptohopper-dashboard-watchlist/raw/main/all.user.js
 // @match        https://www.cryptohopper.com/*
 // @icon         https://www.google.com/s2/favicons?domain=cryptohopper.com
-// @require      https://github.com/eatsleepcoderepeat-gl/cryptohopper-dashboard-watchlist/raw/dev/master-script/coin-watchlist.user.js
-// @require      https://github.com/eatsleepcoderepeat-gl/cryptohopper-dashboard-watchlist/raw/dev/master-script/absolute-value.user.js
-// @require      https://github.com/eatsleepcoderepeat-gl/cryptohopper-dashboard-watchlist/raw/dev/master-script/chart-mods.user.js
-// @require      https://github.com/eatsleepcoderepeat-gl/cryptohopper-dashboard-watchlist/raw/dev/master-script/position-targets.user.js
-// @require      https://github.com/eatsleepcoderepeat-gl/cryptohopper-dashboard-watchlist/raw/dev/master-script/target-restore.user.js
-// @require      https://github.com/eatsleepcoderepeat-gl/cryptohopper-dashboard-watchlist/raw/dev/master-script/remove-hoppie.user.js
-// @require      https://github.com/eatsleepcoderepeat-gl/cryptohopper-dashboard-watchlist/raw/dev/master-script/stay-level-headed.user.js
-// @require      https://github.com/eatsleepcoderepeat-gl/cryptohopper-dashboard-watchlist/raw/dev/master-script/multi-select.user.js
-// @require      https://github.com/eatsleepcoderepeat-gl/cryptohopper-dashboard-watchlist/raw/dev/master-script/export-saved-trade-history.user.js
+// @require      https://github.com/markrickert/cryptohopper-dashboard-watchlist/raw/main/coin-watchlist.user.js
+// @require      https://github.com/markrickert/cryptohopper-dashboard-watchlist/raw/main/absolute-value.user.js
+// @require      https://github.com/markrickert/cryptohopper-dashboard-watchlist/raw/main/chart-mods.user.js
+// @require      https://github.com/markrickert/cryptohopper-dashboard-watchlist/raw/main/position-targets.user.js
+// @require      https://github.com/markrickert/cryptohopper-dashboard-watchlist/raw/main/target-restore.user.js
+// @require      https://github.com/markrickert/cryptohopper-dashboard-watchlist/raw/main/remove-hoppie.user.js
+// @require      https://github.com/markrickert/cryptohopper-dashboard-watchlist/raw/main/stay-level-headed.user.js
+// @require      https://github.com/markrickert/cryptohopper-dashboard-watchlist/raw/main/multi-select.user.js
+// @require      https://github.com/markrickert/cryptohopper-dashboard-watchlist/raw/main/export-saved-trade-history.user.js
 // @grant        GM_addStyle
 // @grant        GM_getValue
 // @grant        GM_listValues
