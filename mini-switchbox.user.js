0c4701a9d679b107522e147cde182aa5599c7bba
falcontx <donny@donnycryer.com>
2021-06-02 19:31:26 -0600

    Update mini-switchbox.user.js

--- mini-switchbox.user.js
+++ mini-switchbox.user.js
@@ -12,7 +12,10 @@
 (function() {
     'use strict';
 
-    jQuery("#component_content > div:nth-child(4) > div.col-lg-4 > div.card-box").css("zoom", "0.9").prop('id', 'switchbox').css("padding-bottom", "5px");;
+    jQuery("#component_content > div:nth-child(4) > div.col-lg-4 > div.card-box").prop('id', 'switchbox').css("padding-bottom", "5px");
+    var w = jQuery("#switchbox").width();
+    var z = w <= 320 ? w/320*0.9 : 0.9;
+    jQuery("#switchbox").css("zoom", z);
     jQuery("#switchbox > div").css("display", "inline-block").css("margin-bottom", "15px");
     jQuery("#switchbox > hr").css("display", "none");
     jQuery("#switchbox > div > .wid-icon-info").css("margin-left", "auto");
